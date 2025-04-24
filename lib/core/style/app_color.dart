a97ee235082dc8ceb0885ee5abaff462ddba6b1d
Ammar <amarabublalla90@gmail.com>
2025-04-25 02:35:34 +0300

    style:app_color

---
+++ lib/core/style/app_color.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color primaryColor = Color.fromRGBO(0, 79, 98, 1);
+  static const Color secondaryColor = Color.fromRGBO(255, 255, 255, 1);
+  static const Color textColor = Color.fromRGBO(0, 0, 0, 1);
+  static const Color greenColor = Color.fromRGBO(203, 255, 203, 1);
+}
