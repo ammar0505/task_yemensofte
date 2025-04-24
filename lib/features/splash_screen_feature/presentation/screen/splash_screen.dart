22aa1b59f7a66f82eeb185d6d9715cb6fb101943
Ammar <amarabublalla90@gmail.com>
2025-04-25 02:36:51 +0300

    update:add timer

--- lib/features/splash_screen_feature/presentation/screen/splash_screen.dart
+++ lib/features/splash_screen_feature/presentation/screen/splash_screen.dart
@@ -1,8 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_yemensofte/features/delivery_login_feature/presentation/screen/delivery_login_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => DeliveryLoginScreen(),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +38,12 @@ class SplashScreen extends StatelessWidget {
           alignment: Alignment.center,
           children: [
             Positioned(
-              top: MediaQuery.of(context).size.height * 0.3      ,
+              top: MediaQuery.of(context).size.height * 0.3,
               child: SvgPicture.asset(
                 'images/Group 3915.svg',
                 height: 112,
                 width: 276,
-              ),  
+              ),
             ),
             Positioned(
               bottom: 25,
