7544c660364d83963d83d6dbf753e3527b439474
Ammar <amarabublalla90@gmail.com>
2025-04-25 05:53:13 +0300

    update

--- lib/core/use_cases/param_use_case.dart
+++ lib/core/use_cases/param_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:task_yemensofte/core/errors/failure.dart';
+
 abstract class UseCaseWithParam<type, params> {
   Future<Either<Failure, type>> execute(params params);
 }
