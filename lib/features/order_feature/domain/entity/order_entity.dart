6a69f533cd862660c0cc94d9bcfd8508b9bb5c1a
Ammar <amarabublalla90@gmail.com>
2025-04-25 14:47:42 +0300

    add list DeliveryBill(sqilflite)

--- lib/features/order_feature/domain/entity/order_entity.dart
+++ lib/features/order_feature/domain/entity/order_entity.dart
@@ -1,11 +1,15 @@
+import 'package:task_yemensofte/features/order_feature/data/model/order_model/order_model/delivery_bill.dart';
+
 class OrderEntity {
   final String? id;
   final String? total;
   final String? date;
+  final List<DeliveryBill>? deliveryBills;
 
   OrderEntity({
     required this.id,
     required this.total,
     required this.date,
+    this.deliveryBills,
   });
 }
