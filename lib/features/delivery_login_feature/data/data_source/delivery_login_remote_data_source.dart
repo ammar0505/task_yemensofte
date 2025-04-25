344787db1c41e506f080291842ade0e84de29f36
Ammar <amarabublalla90@gmail.com>
2025-04-25 07:09:05 +0300

    update

--- lib/features/delivery_login_feature/data/data_source/delivery_login_remote_data_source.dart
+++ lib/features/delivery_login_feature/data/data_source/delivery_login_remote_data_source.dart
@@ -1,31 +1,36 @@
-
 import 'package:task_yemensofte/core/api_service.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/data/model/delivery_login_model/delivery_login_model.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/domain/entity/delivery_login_entity.dart';
 
 abstract class DeliveryLoginRemoteDataSource {
-  Future<DeliveryLoginEntity> signIn(String userid, String password,String lang);
-
+  Future<DeliveryLoginEntity> login(
+      String userId, String password, String lang);
 }
 
-class DeliveryLoginRemoteDataSourceImpl implements DeliveryLoginRemoteDataSource {
+class DeliveryLoginRemoteDataSourceImpl
+    implements DeliveryLoginRemoteDataSource {
   final ApiService apiService;
-  DeliveryLoginRemoteDataSourceImpl(this.apiService,);
 
-  
+  DeliveryLoginRemoteDataSourceImpl(this.apiService);
 
   @override
-  Future<DeliveryLoginEntity> signIn(String userid, String password,String lang) async {      
-    var data = await apiService.post(
-      endPoint: "OnyxDeliveryService/Service.svc/CheckDeliveryLogin",
-      data: { "P_LANG_NO": lang,
-        "P_DLVRY_NO": userid,
-        "P_PSSWRD": password},
-    );
+  Future<DeliveryLoginEntity> login(
+      String userId, String password, String lang) async {
+    try {
+      final response = await apiService.post(
+        endPoint: 'Service.svc/CheckDeliveryLogin',
+        data: {
+          "Value": {
+        "P_LANG_NO": "2",
+        "P_DLVRY_NO": userId,
+        "P_PSSWRD": password
+    }
+        },
+      );
 
-    DeliveryLoginEntity userVerification = DeliveryLoginModel.fromJson(data);
-
-
-    return userVerification;
+      return DeliveryLoginModel.fromJson(response);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
