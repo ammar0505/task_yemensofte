43267bb674ce7120497cbb6e62d120ebe3112024
Ammar <amarabublalla90@gmail.com>
2025-04-25 14:44:20 +0300

    add registerSingleton

--- lib/core/setup_service_locator.dart
+++ lib/core/setup_service_locator.dart
@@ -6,6 +6,8 @@ import 'package:task_yemensofte/core/api_service.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/data/data_source/delivery_login_remote_data_source.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/data/repo/delivery_login_repoimpl.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/domain/repo/delivery_login_repo.dart';
+import 'package:task_yemensofte/features/order_feature/data/data_source/order_data_source.dart';
+import 'package:task_yemensofte/features/order_feature/data/repo/order_repoimpl.dart';
 
 final getit = GetIt.instance;
 
@@ -15,6 +17,12 @@ void setupServiceLocator() {
   getit.registerSingleton<DeliveryLoginRepoimpl>(DeliveryLoginRepoimpl(
     deliveryLoginRemoteDataSource: 
           DeliveryLoginRemoteDataSourceImpl(getit.get<ApiService>())
+  )
+    
+  );
+  getit.registerSingleton<OrderRepoimpl>(OrderRepoimpl(
+    orderRemoteDataSource: 
+          OrderRemoteDataSourceImpl(getit.get<ApiService>())
   )
     
   );
