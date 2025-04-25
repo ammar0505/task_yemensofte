0610b4b145d0160001cab936663b8460e1c5bb5f
Ammar <amarabublalla90@gmail.com>
2025-04-25 05:56:07 +0300

    update

--- lib/features/delivery_login_feature/domain/usecase/delivery_login_usecase.dart
+++ lib/features/delivery_login_feature/domain/usecase/delivery_login_usecase.dart
@@ -1,18 +1,20 @@
-
 import 'package:dartz/dartz.dart';
 import 'package:task_yemensofte/core/errors/failure.dart';
 import 'package:task_yemensofte/core/use_cases/param_use_case.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/domain/entity/delivery_login_entity.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/domain/repo/delivery_login_repo.dart';
 
-class DeliveryLoginUsecase extends UseCaseWithParam<DeliveryLoginEntity, DeliveryLoginParams> {
+class DeliveryLoginUsecase
+    extends UseCaseWithParam<DeliveryLoginEntity, DeliveryLoginParams> {
   final DeliveryLoginRepo deliveryLoginRepo;
 
   DeliveryLoginUsecase(this.deliveryLoginRepo);
 
   @override
-  Future<Either<Failure, DeliveryLoginEntity>> execute(DeliveryLoginParams params) {
-    return deliveryLoginRepo.signIn(params.userid, params.password, params.lang);
+  Future<Either<Failure, DeliveryLoginEntity>> execute(
+      DeliveryLoginParams params) {
+    return deliveryLoginRepo.signIn(
+        params.userid, params.password, params.lang);
   }
 }
 
