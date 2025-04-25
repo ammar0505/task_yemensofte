4aafb38b60915d62c6511343f6dd5253e801d9db
Ammar <amarabublalla90@gmail.com>
2025-04-25 14:52:53 +0300

    add BlocProvider and add InactivityWrapper

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:provider/provider.dart';
 import 'package:task_yemensofte/core/provider/order_state.dart';
 import 'package:task_yemensofte/core/setup_service_locator.dart';
 import 'package:task_yemensofte/core/style/app_color.dart';
+import 'package:task_yemensofte/core/widgets/inactivity_wrapper.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/data/repo/delivery_login_repoimpl.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/domain/usecase/delivery_login_usecase.dart';
 import 'package:task_yemensofte/features/delivery_login_feature/presentation/manger/cubit/delivery_login_cubit_cubit.dart';
-import 'package:task_yemensofte/features/order_feature/presentation/screen/order_screen.dart';
+import 'package:task_yemensofte/features/order_feature/data/repo/order_repoimpl.dart';
+import 'package:task_yemensofte/features/order_feature/domain/usecase/order_usecase.dart';
+import 'package:task_yemensofte/features/order_feature/presentation/manger/cubit/order_cubit_cubit.dart';
 import 'package:task_yemensofte/features/splash_screen_feature/presentation/screen/splash_screen.dart';
 
 void main() {
@@ -16,12 +19,17 @@ void main() {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => OrderStatusProvider()),
-  BlocProvider(
-          create:
-              (context) => DeliveryLoginCubit(
-                DeliveryLoginUsecase(getit.get<DeliveryLoginRepoimpl>()),
-              ),
-        ),      ],
+        BlocProvider(
+          create: (context) => DeliveryLoginCubit(
+            DeliveryLoginUsecase(getit.get<DeliveryLoginRepoimpl>()),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => OrderCubitCubit(
+            OrderUsecase(getit.get<OrderRepoimpl>()),
+          ),
+        )
+      ],
       child: const MyApp(),
     ),
   );
@@ -41,7 +49,9 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: InactivityWrapper(
+        child: const SplashScreen(),
+      ),
     );
   }
 }
