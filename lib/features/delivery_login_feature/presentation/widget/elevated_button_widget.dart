e21cade1329c6c3741cf35cb6e28b7f8686aafca
Ammar <amarabublalla90@gmail.com>
2025-04-25 05:56:21 +0300

    update

--- lib/features/delivery_login_feature/presentation/widget/elevated_button_widget.dart
+++ lib/features/delivery_login_feature/presentation/widget/elevated_button_widget.dart
@@ -5,34 +5,35 @@ class ElevatedButtonWidget extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
 
-  const ElevatedButtonWidget({super.key, required this.text, required this.onPressed});
+  const ElevatedButtonWidget(
+      {super.key, required this.text, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-              child: Container(
-                width: 342,
-                height: 44,
+    return Center(
+      child: Container(
+        width: 342,
+        height: 44,
         decoration: BoxDecoration(
           color: AppColor.primaryColor,
           borderRadius: BorderRadius.circular(30),
         ),
-                child: ElevatedButton(
-                  onPressed: onPressed,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColor.primaryColor,
-                  ),
-                  child: Text(
-                    text,
-                    style: const TextStyle(
-                      color: AppColor.secondaryColor,
-                      fontSize: 16,
-                      fontFamily: 'Montserrat',
-                      // fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-            );
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColor.primaryColor,
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: AppColor.secondaryColor,
+              fontSize: 16,
+              fontFamily: 'Montserrat',
+              // fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
