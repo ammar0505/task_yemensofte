17ff1d4e2503001e704bb4839b668a0dbb1b668b
Ammar <amarabublalla90@gmail.com>
2025-04-25 11:23:56 +0300

    usecase

---
+++ lib/features/order_feature/domain/usecase/order_usecase.dart
@@ -0,0 +1,28 @@
+
+            
+import 'package:dartz/dartz.dart';
+import 'package:task_yemensofte/core/errors/failure.dart';
+import 'package:task_yemensofte/core/use_cases/param_use_case.dart';
+import 'package:task_yemensofte/features/order_feature/domain/entity/order_entity.dart';
+import 'package:task_yemensofte/features/order_feature/domain/repo/order_repo.dart';
+
+class OrderUsecase
+    extends UseCaseWithParam<OrderEntity, OrderParams> {
+  final OrderRepo orderRepo;
+
+  OrderUsecase(this.orderRepo);
+
+  @override
+  Future<Either<Failure, OrderEntity>> execute(OrderParams params) {
+    return orderRepo.getOrders();
+  }
+}
+
+class OrderParams {
+  final String userid;
+  final String lang;
+  final String P_BILL_SRL;
+  final String P_PRCSSD_FLG;
+
+  OrderParams(this.userid, this.P_BILL_SRL  ,this.lang, this.P_PRCSSD_FLG);
+}
