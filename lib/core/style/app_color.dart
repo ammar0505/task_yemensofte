38bec972b87f538b751f932ac00372cf6358e048
Ammar <amarabublalla90@gmail.com>
2025-04-25 05:53:04 +0300

    update

--- lib/core/style/app_color.dart
+++ lib/core/style/app_color.dart
@@ -5,9 +5,8 @@ class AppColor {
   static const Color secondaryColor = Color.fromRGBO(255, 255, 255, 1);
   static const Color textColor = Color.fromRGBO(0, 0, 0, 1);
   static const Color greenColor = Color.fromRGBO(203, 255, 203, 1);
-  static const Color redColor = Color.fromRGBO(212, 42, 15, 1);    
-  static const Color brownColor = Color.fromRGBO( 128, 128, 128, 1);
+  static const Color redColor = Color.fromRGBO(212, 42, 15, 1);
+  static const Color brownColor = Color.fromRGBO(128, 128, 128, 1);
   static const Color darkGreenColor = Color.fromRGBO(41, 212, 15, 1);
   static const Color lightGrayColor = Color.fromRGBO(112, 112, 112, 1);
-
 }
